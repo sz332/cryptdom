01600e945c8997c9fa757381ed8979e38c04d5ab
Zsolt Sandor <zsolt.sandor@gmail.com>
2019-02-03 16:36:24 +0100

    added unit test to code

--- src/test/java/hu/acme/cryptodom/SignedDocumentTest.java
+++ src/test/java/hu/acme/cryptodom/SignedDocumentTest.java
@@ -12,6 +12,8 @@ import hu.acme.cryptodom.keystore.KeyRepository;
 // https://www.oracle.com/technetwork/articles/javase/dig-signature-api-140772.html
 // https://www.w3.org/TR/xmldsig-core/
 // https://docs.oracle.com/javase/8/docs/api/javax/xml/crypto/dom/package-frame.html
+// http://www.bouncycastle.org/wiki/display/JA1/X.509+Public+Key+Certificate+and+Certification+Request+Generation
+// https://www.ibm.com/support/knowledgecenter/en/SSYKE2_7.0.0/com.ibm.java.security.component.70.doc/security-component/keytoolDocs/x500dnames.html
 public class SignedDocumentTest {
 
     private static final String ALIAS = "alias";
